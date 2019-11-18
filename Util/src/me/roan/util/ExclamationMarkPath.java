dde06bddff8560c78b6eedcdffd9e1c1d1593b9d
Roan Hofland <roan.hofland@hotmail.com>
2019-11-18 02:16:09 +0100

    Some more work

--- Util/src/me/roan/util/ExclamationMarkPath.java
+++ Util/src/me/roan/util/ExclamationMarkPath.java
@@ -1,28 +1,13 @@
 package me.roan.util;
 
-import java.awt.Window.Type;
-import java.io.BufferedReader;
 import java.io.File;
-import java.io.FileOutputStream;
 import java.io.IOException;
-import java.io.InputStream;
-import java.io.InputStreamReader;
-import java.io.OutputStream;
-import java.lang.reflect.Field;
-import java.net.MalformedURLException;
+import java.lang.ProcessBuilder.Redirect;
 import java.net.URISyntaxException;
 import java.net.URL;
-import java.net.URLClassLoader;
 import java.nio.file.Files;
-import java.nio.file.Path;
 import java.nio.file.StandardCopyOption;
-import java.nio.file.StandardOpenOption;
 import java.security.CodeSource;
-import java.util.Enumeration;
-import java.util.jar.JarEntry;
-import java.util.jar.JarFile;
-import java.util.zip.ZipEntry;
-import java.util.zip.ZipFile;
 
 /**
  * Currently as of writing this class there is a bug in the JDK where
@@ -32,13 +17,17 @@ import java.util.zip.ZipFile;
  * of times with the first report I could find being from the 9th of 
  * August, 2000 (see links below). 
  * <p>
- * The cause of this iss
- * 
- * 
- * 
- *  
- * 
- * 
+ * The cause of this issue is with the {@linkplain java.net.JarURLConnection}
+ * class. However, since this is a <code>java.*</code> it cannot be overridden
+ * at run time using for example a custom class loader. In addition to this
+ * distributing a fixed copy of the class would be breaking the license agreement.
+ * <p>
+ * Other possible fixes like using a custom system class loader do not work
+ * for similar reasons since certain resources get loaded using the bootstrap
+ * class loader so that one fails...
+ * <p>
+ * If there ever comes a time when I can delete this work around class I'll
+ * be happy, but it's been a bug for almost 20 years as of writing this...
  * @author Roan
  * @see <a href="https://bugs.openjdk.java.net/browse/JDK-4361044">JDK Bug 4361044</a>
  * @see <a href="https://bugs.openjdk.java.net/browse/JDK-4523159">JDK Bug 4523159</a>
@@ -51,12 +40,36 @@ import java.util.zip.ZipFile;
 public class ExclamationMarkPath{
 	
 	private static File exe;
+	
+	public static void handleSituation(String args){
+		if(check()){
+			showWarning(args);
+		}
+	}
+	
+	private static final void showWarning(String args){
+		
+		
+		//TODO show warning and info dialog and provide the option to relaunch from temp
+		
+		
+		
+		//RL anyway
+		relaunchFromTemp(args);
+		Dialog.showMessageDialog("An error occured whilst trying to launch the program >.<");
 
-	public static final boolean check(){
+	}
+
+	/**
+	 * Checks if the current executable location
+	 * is affected by the JDK bug.
+	 * @return True if the current location is affected.
+	 */
+	private static final boolean check(){
 		CodeSource source = ExclamationMarkPath.class.getProtectionDomain().getCodeSource();
 		if(source == null){
 			//Chances of there being no problem are higher
-			return true;
+			return false;
 		}
 		
 		//First get the exe location
@@ -67,76 +80,67 @@ public class ExclamationMarkPath{
 			exe = new File(url.getPath());
 		}
 		
-		//If there is no exclamation mark there is no problem
-		return !exe.getAbsolutePath().contains("!");
+		//If there is an exclamation mark there is a problem
+		return exe.getAbsolutePath().contains("!");
 	}
-	
-	
-	
-	
 
 	/**
 	 * Re-launches the program from the temp directory
-	 * if the program path contains a ! this fixes a
-	 * bug in the native library loading
+	 * if the program path contains a ! this fixes the
+	 * JDK bug. If this subroutine managed to execute
+	 * properly the call to this subroutine will never
+	 * return.
 	 * @param args The original command line arguments
+	 *        <code>null</code> allowed for none.
 	 */
 	private static final void relaunchFromTemp(String args){
-		
-		File jvm = new File(System.getProperty("java.home") + File.separator + "bin" + File.separator + "java.exe");
-		if(!jvm.exists() || !exe.exists()){
-			System.out.println("JVM exists: " + jvm.exists() + " Executable exists: " + exe.exists());
-			Dialog.showMessageDialog("An error occured whilst trying to launch the program >.<");
-			System.exit(0);
+		if(!exe.exists()){
+			//All hope lost
+			return;
 		}
+		
+		//Assume java isn't on the path
+		File java = new File(System.getProperty("java.home") + File.separator + "bin" + File.separator + "java.exe");
+		String jvm;
+		if(java.exists()){
+			jvm = java.getAbsolutePath();
+		}else{
+			//Hope java is on the path
+			jvm = "java";
+		}
+		
+		//Attempt to copy the program to the temp directory
 		File tmp = null;
 		try{
-			tmp = File.createTempFile("kps", null);
+			tmp = File.createTempFile(null, null);
+			tmp.deleteOnExit();
+
+			//Infinite loops are no fun
 			if(tmp.getAbsolutePath().contains("!")){
-				Dialog.showMessageDialog("An error occured whilst trying to launch the program >.<");
-				System.exit(0);
+				return;
 			}
+			
 			Files.copy(exe.toPath(), tmp.toPath(), StandardCopyOption.REPLACE_EXISTING);
 		}catch(IOException e){
 			e.printStackTrace();
-			Dialog.showMessageDialog("An error occured whilst trying to launch the program >.<");
-			tmp.deleteOnExit();
-			tmp.delete();
-			System.exit(0);
+			return;
 		}
+		
+		//Build the new process
 		ProcessBuilder builder = new ProcessBuilder();
 		if(args != null){
-			builder.command(jvm.getAbsolutePath(), "-jar", tmp.getAbsolutePath(), args);
+			builder.command(jvm, "-jar", tmp.getAbsolutePath(), args);
 		}else{
-			builder.command(jvm.getAbsolutePath(), "-jar", tmp.getAbsolutePath());
+			builder.command(jvm, "-jar", tmp.getAbsolutePath());
 		}
-		Process proc = null;
+		builder.redirectOutput(Redirect.INHERIT);
+		builder.redirectError(Redirect.INHERIT);
+		
+		//Start an hope for the best
 		try{
-			proc = builder.start();
-		}catch(IOException e){
+			System.exit(builder.start().waitFor());
+		}catch(IOException | InterruptedException e){
 			e.printStackTrace();
-			Dialog.showMessageDialog("An error occured whilst trying to launch the program >.<");
-			tmp.deleteOnExit();
-			tmp.delete();
-			System.exit(0);
 		}
-		BufferedReader in = new BufferedReader(new InputStreamReader(proc.getInputStream()));
-		String line;
-		try{
-			while(proc.isAlive()){
-				while((line = in.readLine()) != null){
-					System.out.println(line);
-				}
-				try{
-					Thread.sleep(1000);
-				}catch(InterruptedException e){
-				}
-			}
-		}catch(IOException e){
-			System.err.print("Output stream chrashed :/");
-		}
-		tmp.deleteOnExit();
-		tmp.delete();
-		System.exit(0);
 	}
 }
