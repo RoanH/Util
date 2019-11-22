3ffdb09f761c1dd64915d4d6abd096ab5e758365
Roan Hofland <roan.hofland@hotmail.com>
2019-11-22 01:34:26 +0100

    Refactor dialogues

--- Util/native/me_roan_util_FileSelector.cpp
+++ Util/native/me_roan_util_FileSelector.cpp
@@ -9,44 +9,51 @@
 #define FILES 8
 #define FOLDERS 16
 
-LPWSTR showOpenFileDialog(){
+LPWSTR showOpenFileDialog(int flags){
+	LPWSTR path;
 	HRESULT hr = CoInitializeEx(NULL, COINIT_APARTMENTTHREADED | COINIT_DISABLE_OLE1DDE);
 	if(SUCCEEDED(hr)){
-		IFileSaveDialog* openDialog;
-		hr = CoCreateInstance(CLSID_FileSaveDialog, NULL, CLSCTX_ALL, IID_IFileSaveDialog, reinterpret_cast<void**>(&openDialog));
+		IFileDialog* dialog;
+		if((flags & SAVE) > 0){
+			hr = CoCreateInstance(CLSID_FileSaveDialog, NULL, CLSCTX_ALL, IID_IFileSaveDialog, reinterpret_cast<void**>(&dialog));
+		}else{
+			hr = CoCreateInstance(CLSID_FileOpenDialog, NULL, CLSCTX_ALL, IID_IFileOpenDialog, reinterpret_cast<void**>(&dialog));
+		}
 
 		if(SUCCEEDED(hr)){
-			DWORD options;
-			if(SUCCEEDED(openDialog->GetOptions(&options))){
-				openDialog->SetOptions(options);//
-			}
-
-			// Show the Open dialog box.
-			hr = openDialog->Show(NULL);
-
-			// Get the file name from the dialog box.
-			if(SUCCEEDED(hr)){
-				IShellItem* pItem;
-				hr = openDialog->GetResult(&pItem);
-				if(SUCCEEDED(hr)){
-					LPWSTR pszFilePath;
-					hr = pItem->GetDisplayName(SIGDN_FILESYSPATH, &pszFilePath);
-
-					CoTaskMemFree(pszFilePath);
-					wprintf(pszFilePath);
-					pItem->Release();
+			if((flags & FOLDERS) > 0){
+				DWORD options;
+				if(SUCCEEDED(dialog->GetOptions(&options))){
+					dialog->SetOptions(options | FOS_PICKFOLDERS);
 				}
 			}
-			openDialog->Release();
+			
+			hr = dialog->Show(NULL);
+			if(SUCCEEDED(hr)){
+				IShellItem* item;
+				hr = dialog->GetResult(&item);
+				if(SUCCEEDED(hr)){
+					item->GetDisplayName(SIGDN_FILESYSPATH, &path);
+					item->Release();
+				}
+			}
+			dialog->Release();
 		}
 		CoUninitialize();
 	}
-	return 0;
+	return path;
+}
+
+jstring toString(JNIEnv* env, LPWSTR data){
+
+
+	CoTaskMemFree(data);
+	return NULL;
 }
 
 JNIEXPORT jstring JNICALL Java_me_roan_util_FileSelector_showNativeFileOpen(JNIEnv *env, jclass obj){
 	//showOpenDialog(0);
-	showOpenFileDialog();
+	showOpenFileDialog(0);
 
 
 
