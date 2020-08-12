9a1d81bc1da293aa2a1621c4325cab640ef7f207
Roan Hofland <roan.hofland@hotmail.com>
2020-08-12 19:12:40 +0200

    Native code cleanup

--- Util/native/me_roan_util_FileSelector.cpp
+++ Util/native/me_roan_util_FileSelector.cpp
@@ -23,8 +23,15 @@ typedef struct{
 FILE_TYPE *extensions;
 int ext_num = 0;
 
-//Shows a dialog according to the passed flags (see definitions)
-LPWSTR showDialog(int flags, long types, long typec){
+/**
+ * Shows a dialog according to the passed flags (see definitions).
+ * @param flags Determines whether to show a SAVE or OPEN dialog 
+ *        and FILES or FOLDER selection.
+ * @param types Bitwise combination of file extension filters to enable.
+ * @param typec Number of bits set in types.
+ * @param fname Default save file name, NULL for an OPEN dialog.
+ */
+LPWSTR showDialog(int flags, long types, long typec, LPWSTR fname){
 	LPWSTR path = NULL;
 	HRESULT hr = CoInitializeEx(NULL, COINIT_APARTMENTTHREADED | COINIT_DISABLE_OLE1DDE);
 	if(SUCCEEDED(hr)){
@@ -37,52 +44,43 @@ LPWSTR showDialog(int flags, long types, long typec){
 		}
 
 		if(SUCCEEDED(hr)){
-			//if((flags & FOLDERS) > 0){
-				DWORD options;
-				if(SUCCEEDED(dialog->GetOptions(&options))){
-					if((flags & FOLDERS) > 0){
+			DWORD options;
+			if(SUCCEEDED(dialog->GetOptions(&options))){
+				if((flags & FOLDERS) > 0){
 					dialog->SetOptions(options | FOS_PICKFOLDERS);
-					} else if(typec != 0){
-						printf("Set strict\n");
-					}
 				}
-			//}
-			
-			if(typec != 0){
-				filters = (COMDLG_FILTERSPEC*)malloc(typec * sizeof(COMDLG_FILTERSPEC));
-				if(filters != NULL){
-					int idx = 0;
-					for(long i = 0; idx < typec; i++){
-						if((types & (1 << i)) != 0){
-							filters[idx++] = extensions[i].ext;
+
+				if(typec != 0){
+					filters = (COMDLG_FILTERSPEC*)malloc(typec * sizeof(COMDLG_FILTERSPEC));
+					if(filters != NULL){
+						int idx = 0;
+						for(long i = 0; idx < typec; i++){
+							if((types & (1 << i)) != 0){
+								filters[idx++] = extensions[i].ext;
+							}
 						}
-					}
-					dialog->SetFileTypes(typec, filters);
-					hr = dialog->SetOptions(options | FOS_STRICTFILETYPES);
-					if(SUCCEEDED(hr)){
-						printf("The world has ended and windows lives\n");
-					}else{
-						printf("It is even worse\n");
+						dialog->SetFileTypes(typec, filters);
+						dialog->SetOptions(options | FOS_STRICTFILETYPES);
 					}
 				}
-			}
 
-			//TODO pass as argument
-			//hr = dialog->SetFileName(L"test.png");
+				//TODO pass as argument
+				//hr = dialog->SetFileName(L"test.png");
 
-			//Force an extension, this being the default
-			dialog->SetDefaultExtension(L"png;");
-			
-			hr = dialog->Show(NULL);
-			if(SUCCEEDED(hr)){
-				IShellItem *item;
-				hr = dialog->GetResult(&item);
+				//Force an extension, this being the default
+				dialog->SetDefaultExtension(L"png");
+
+				hr = dialog->Show(NULL);
 				if(SUCCEEDED(hr)){
-					item->GetDisplayName(SIGDN_FILESYSPATH, &path);
-					item->Release();
+					IShellItem* item;
+					hr = dialog->GetResult(&item);
+					if(SUCCEEDED(hr)){
+						item->GetDisplayName(SIGDN_FILESYSPATH, &path);
+						item->Release();
+					}
 				}
+				dialog->Release();
 			}
-			dialog->Release();
 		}
 		CoUninitialize();
 		if(filters != NULL){
@@ -118,17 +116,17 @@ jstring toString(JNIEnv *env, LPWSTR data){
 
 //Native subroutine for me.roan.util.FileSelector#showNativeFileOpen
 JNIEXPORT jstring JNICALL Java_me_roan_util_FileSelector_showNativeFileOpen(JNIEnv *env, jclass obj, jint types, jint typec){
-	return toString(env, showDialog(FILES | OPEN, types, typec));
+	return toString(env, showDialog(FILES | OPEN, types, typec, NULL));
 }
 
 //Native subroutine for me.roan.util.FileSelector#showNativeFolderOpen
 JNIEXPORT jstring JNICALL Java_me_roan_util_FileSelector_showNativeFolderOpen(JNIEnv *env, jclass obj){
-	return toString(env, showDialog(FOLDERS | OPEN, 0, 0));
+	return toString(env, showDialog(FOLDERS | OPEN, 0, 0, NULL));
 }
 
 //Native subroutine for me.roan.util.FileSelector#showNativeFileSave
 JNIEXPORT jstring JNICALL Java_me_roan_util_FileSelector_showNativeFileSave(JNIEnv *env, jclass obj, jint type){
-	return toString(env, showDialog(FILES | SAVE, type, type == 0 ? 0 : 1));
+	return toString(env, showDialog(FILES | SAVE, type, type == 0 ? 0 : 1, NULL));//TODO
 }
 
 //Native subroutine for me.roan.util.FileSelector#registerFileExtension
